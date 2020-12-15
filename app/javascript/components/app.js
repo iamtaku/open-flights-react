86564e2fd69b6973a98fbcb8f230e2117b83f948
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-15 14:32:37 +0900

    added react router dom + added test component file

---
+++ app/javascript/components/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+// import Airlines from "./airlines/airlines";
+// import Airline from "./airline/airline";
+import Test from "./test";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Test} />
+      {/* <Route exact path="/" component={Airlines} /> */}
+      {/* <Route exact path="/airlines/:slug" component={Airline} /> */}
+    </Switch>
+  );
+};
+
+export default App;
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,26 +1,13 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../components/app";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
