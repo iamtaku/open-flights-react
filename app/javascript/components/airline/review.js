552632558a7eeb9f822b0913aff5bb131fe51707
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-15 19:03:01 +0900

    added styling to each review + added star rating component

--- app/javascript/components/airline/review.js
+++ app/javascript/components/airline/review.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Rating from "../rating/rating";
 
 const Card = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.1);
@@ -7,7 +8,10 @@ const Card = styled.div`
   padding: 20px;
   margin: 0 20px 20px 0;
 `;
-const RatingContainer = styled.div``;
+const RatingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 const RatingScoreContainer = styled.div``;
 const Title = styled.div`
   padding: 20px 0 0 0;
@@ -22,9 +26,9 @@ const Review = (props) => {
   const { score, title, description } = props.attributes;
   return (
     <Card>
-      <div className="rating-container">
-        <div className="rating-score">{score}</div>
-      </div>
+      <RatingContainer className="rating-container">
+        <Rating score={score} />
+      </RatingContainer>
       <Title className="title">{title}</Title>
       <Description className="description">{description}</Description>
     </Card>
--- app/javascript/components/airlines/gridItem.js
+++ app/javascript/components/airlines/gridItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import styled from "styled-components";
+import Rating from "../rating/rating";
 
 const Card = styled.div`
   border: 1px solid #efefef;
@@ -42,7 +43,7 @@ const AirlineGridItem = (props) => {
         <img src={props.attributes.image_url} alt={props.attributes.name} />
       </AirlineLogo>
       <AirlineName>{props.attributes.name}</AirlineName>
-      <div className="airline-score">{props.attributes.avg_score}</div>
+      <Rating score={props.attributes.avg_score}></Rating>
       <LinkWrapper>
         <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
       </LinkWrapper>
