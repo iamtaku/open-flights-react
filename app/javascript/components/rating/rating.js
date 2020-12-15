bebe0a2d26a01aff1bcba66cc1f4e1fe2792b296
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-15 18:43:49 +0900

    added reviews show page template on airline show page

---
+++ app/javascript/components/rating/rating.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rating = (props) => {
+  return (
+    <>
+      <span className="star-wrapper"></span>
+      <span className="stars"></span>
+    </>
+  );
+};
+
+export default Rating;
