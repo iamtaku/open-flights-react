5b81b4c64f6dbc06eb0f9d107e46fa579792985f
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-15 22:23:47 +0900

    finish api auth setup and configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   namespace :api do
     namespace :v1 do
       resources :airlines, param: :slug
       resources :reviews, only:[:create, :destroy]
+      mount_devise_token_auth_for 'User', at: 'auth'
+
     end
   end
   get '*path', to: 'pages#index', via: :all
